2c184f666b91dc8e8b17549324597bf8588f2846
Matias Gomez <matiasgomez@outlook.com>
2025-04-15 10:45:37 -0300

    segundo  asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -34,6 +34,32 @@ string_proc_list_create_asm:
     ret
 
 string_proc_node_create_asm:
+    ; guardo los params que me vienen de la funcion
+    push rdi ;type param
+    push rsi ;hash param
+    ; alineo y llamo malloc
+    sub rsp, 8
+    mov rdi, 32
+    call malloc
+    add rsp, 8
+    ; recupero params de stack a registros
+    pop rsi ;hash param
+    pop rdi ;type param
+    ; si falla malloc
+    test rax, rax
+    je .ret_null
+    ; si no, malloc me devuelve dire asignada en rax
+    xor rdx, rdx
+    mov qword [rax], rdx ;next ptr
+    mov qword [rax+8], rdx ;previous ptr
+    mov byte [rax+8*2], dil ;type param
+    mov qword [rax+8*3], rsi ;hash param
+
+    ret
+
+.ret_null:
+    ret
+
 
 string_proc_list_add_node_asm:
 

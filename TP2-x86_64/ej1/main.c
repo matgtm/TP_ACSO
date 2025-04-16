266f9d0140c698a2c41f8088b07b694c8b18f728
Matias Gomez <matiasgomez@outlook.com>
2025-04-16 10:50:20 -0300

    todo asm

--- TP2-x86_64/ej1/main.c
+++ TP2-x86_64/ej1/main.c
@@ -73,13 +73,13 @@ void test_list_concat(void)
 void run_tests(void){
 
 	/* Aqui pueden comenzar a probar su codigo */
-	test_create_destroy_list();
+	// test_create_destroy_list();
 
-	test_create_destroy_node();
+	// test_create_destroy_node();
 
-	test_create_list_add_nodes();
+	// test_create_list_add_nodes();
 
-	// test_list_concat();
+	test_list_concat();
 }
 
 int main (void){
